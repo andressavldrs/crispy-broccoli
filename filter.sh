efcadbce17e9817211690ac3bbff3a96b4ae301b
andressavldrs <andressarodrial@gmail.com>
2017-10-17 17:18:28 -0200

    Seleciona os tres tamanhos mais frequentes
    Alteracoes no script filter.sh para selecionar os tres tamanhos
    mais frequentes, o script perl select_utr.pl seleciona os ids
    com o tamanho desejado e gera um arquivo de ids com estes.
    No final temos 3 arquivos com as sequencias dos tamanhos mais
    frequentes.

--- filter.sh
+++ filter.sh
@@ -7,32 +7,41 @@ OUT_ID=$3   #output id
 MIN=$4      #min nucleotides value
 
 function utr5 {
-  echo "5'UTR"
+    echo "5'UTR"
 }
 
 function utr3 {
-  echo $XML $OUT_ID $MIN 
-  perl XMLparser.pl $XML $DIR/${OUT_ID}_ids.txt $MIN
+    # echo $XML $OUT_ID $MIN 
+    # parse a XML file and returns a file with ID   UTR LENGTH  CDS POSITION
+    perl XMLparser.pl $XML $DIR/${OUT_ID}_ids.txt $MIN
 
-  perl get_3UTR.pl $SEQ $DIR/utr3_384.txt $DIR/${OUT_ID}_3UTR.fa
-  echo done
-  exit
+    # select the 3 most frequents occurrences and returns files with sequences 
+    <$DIR/${OUT_ID}_ids.txt  awk '{h[$2]++} END { for(k in h) print k, h[k] }' | sort -k2 -nr | 
+    head -n 3 | while read VAR1 VAR2
+    do
+        echo "3'UTR: ${VAR1}nt -> $VAR2"
+        perl select_utr.pl $DIR/${OUT_ID}_ids.txt $VAR1 $DIR/${OUT_ID}_${VAR1}_ids.txt
+        perl get_3UTR.pl $SEQ $DIR/${OUT_ID}_${VAR1}_ids.txt $DIR/${OUT_ID}_${VAR1}_3UTR.fa
+    done
+
+    echo done
+    exit
 }
 
 
 
 echo "Select genome region"
 OPTIONS="5'UTR 3'UTR exit"
-           select opt in $OPTIONS; do 
-               if [ "$opt" = "5'UTR" ]; then
-                utr5
-               elif [ "$opt" = "3'UTR" ]; then
-                utr3  
-               elif [ "$opt" = "exit" ]; then
-                echo done
-                exit
-               else
-                clear
-                echo bad option
-               fi
-           done
+    select opt in $OPTIONS; do 
+        if [ "$opt" = "5'UTR" ]; then
+            utr5
+        elif [ "$opt" = "3'UTR" ]; then
+            utr3  
+        elif [ "$opt" = "exit" ]; then
+            echo done
+            exit
+        else
+            clear
+            echo bad option
+        fi
+    done
