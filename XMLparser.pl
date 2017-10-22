c24e56d821259537116c07afbbaffc3983e13712
andressavldrs <andressarodrial@gmail.com>
2017-10-22 20:36:44 -0200

    Mudancas para verificar se o campo XML existe

--- XMLparser.pl
+++ XMLparser.pl
@@ -13,7 +13,7 @@ use strict;
 use XML::Twig;
 
 my ($xmlfile, $output, $min_utr) = @ARGV;
-
+my $entries = 0;
   open(my $fh, ">", $output)
 or die "Can't open > $output: $!";
 
@@ -22,19 +22,22 @@ my $t = XML::Twig->new(
            twig_roots   => { 'Item' => \&get_data}
                       );
 $t->parsefile( $xmlfile);
-
+print "Number of sequences: $entries\n";
   sub get_data 
     { my( $t, $elt)= @_;
-      my $old_cds_location = $elt->first_child('old_cds_location')->text;    # print the text (including sub-element texts
-      my $length =  $elt->first_child('length')->text;
+      if(($elt->first_child('old_cds_location')) && ($elt->first_child('length'))){
+        my $old_cds_location = $elt->first_child('old_cds_location')->text;    # print the text (including sub-element texts
+        my $length =  $elt->first_child('length')->text;
 
-      $old_cds_location =~ m/gb.(\w*):\d*-(\S*)/; #gb|LC069810:88-10251
+        $old_cds_location =~ m/gb.(\w*):\d*-(\S*)/; #gb|LC069810:88-10251
 
-      my $utr3 = $length - $2; #$total_length - $cds_length
+        my $utr3 = $length - $2; #$total_length - $cds_length
+        $entries++;
+        if($utr3  >= $min_utr){
+          print $fh "$1 $utr3 $2\n";
+        }
 
-      if($utr3  >= $min_utr){
-        print $fh "$1 $utr3 $2\n";
       }
-
+      
       $t->purge;           # frees the memory
     }
