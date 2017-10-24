682362cbd9f74179851c820b1337cd93263d8e98
andressavldrs <andressarodrial@gmail.com>
2017-10-24 16:08:18 -0200

    Mudancas para automatizar o processo para 5'UTR e opcao de ajuda

--- filter.sh
+++ filter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DIR=data/
+DIR=/data
 SEQ=$1 #sequence fasta
 XML=$2      # xml with regions data
 OUT_ID=$3   #output id
@@ -12,8 +12,8 @@ function utr5 {
 
 function utr3 {
     # echo $XML $OUT_ID $MIN 
-    # parse a XML file and returns a file with ID   UTR LENGTH  CDS POSITION
-    perl XMLparser.pl $XML $DIR/${OUT_ID}_ids.txt $MIN
+    # parse a XML file and returns a file with ID   UTR LENGTH  CDS POSITION OP
+    perl XMLparser.pl $XML $DIR/${OUT_ID}_ids.txt $MIN 3
 
     # select the 3 most frequents occurrences and returns files with sequences 
     <$DIR/${OUT_ID}_ids.txt  awk '{h[$2]++} END { for(k in h) print k, h[k] }' | sort -k2 -nr | 
@@ -31,12 +31,14 @@ function utr3 {
 
 
 echo "Select genome region"
-OPTIONS="5'UTR 3'UTR exit"
+OPTIONS="5'UTR 3'UTR help exit"
     select opt in $OPTIONS; do 
         if [ "$opt" = "5'UTR" ]; then
             utr5
         elif [ "$opt" = "3'UTR" ]; then
-            utr3  
+            utr3
+        elif [ "$opt" = "help" ]; then
+            echo HELP
         elif [ "$opt" = "exit" ]; then
             echo done
             exit
